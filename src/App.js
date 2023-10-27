b1b5094e0dfd78e22e813111244e8e55b7c44c05
Ritikchandra <ritik.chandra009@gmail.com>
2023-10-27 13:54:41 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import bgImage from "./bgImage.png"
-import logo from "./logo.png"
-import leftPumpkin from "./leftPumpkin.png"
-import rightPumpkin from "./rightPumpkin.png"
-import shaurya from "./shaurya.png"
-import supreeth from "./supreeth.png"
-import ayush from "./ayush.png"
-import sarthak from "./sarthak.png"
-import vaibhav from "./vaibhav.png"
-import abhinav from "./abhinav.png"
-import prez from "./prez.png"
-import adarsh from "./adarsh.png"
+import bgImage from "./bgImage.png";
+import logo from "./logo.png";
+import leftPumpkin from "./leftPumpkin.png";
+import rightPumpkin from "./rightPumpkin.png";
+import shaurya from "./shaurya.png";
+import supreeth from "./supreeth.png";
+import ayush from "./ayush.png";
+import sarthak from "./sarthak.png";
+import vaibhav from "./vaibhav.png";
+import abhinav from "./abhinav.png";
+import prez from "./prez.png";
+import adarsh from "./adarsh.png";
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 function App() {
@@ -75,11 +75,11 @@ function App() {
       }
     }
 
-    window.addEventListener('keydown', handleKeyPress);
-    
+    window.addEventListener("keydown", handleKeyPress);
+
     // Cleanup the event listener when the component is unmounted
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener("keydown", handleKeyPress);
     };
   }, []); // Empty dependency array ensures the effect runs only once
   const selectedStuccan = data.find((stuccan) => stuccan.id === activeStuccan);
@@ -115,17 +115,22 @@ function App() {
       vaibhav,
       abhinav,
       prez,
-      adarsh
+      adarsh,
     ];
 
     const loadAssets = async () => {
       try {
-        await Promise.all(assets.map((asset) => new Promise((resolve) => {
-          const img = new Image();
-          img.src = asset;
-          img.onload = resolve;
-          img.onerror = resolve; // Handling error case if the image fails to load
-        })));
+        await Promise.all(
+          assets.map(
+            (asset) =>
+              new Promise((resolve) => {
+                const img = new Image();
+                img.src = asset;
+                img.onload = resolve;
+                img.onerror = resolve; // Handling error case if the image fails to load
+              })
+          )
+        );
         setIsLoading(false);
       } catch (error) {
         console.error("Error loading assets:", error);
@@ -134,30 +139,34 @@ function App() {
     };
 
     loadAssets();
-  }, []); 
-  return <>
-  {isLoading && (
-    <div className="loaderWrapper">
-      Loading...
-    </div>
-  )}
-        <div className="pageWrapper">
-          <img src={bgImage} alt="" className="bgImage"/>
-          <div>
-          <img src={logo} alt="Logo" className="logo"/>
-          </div>
-          <motion.img src={leftPumpkin} alt="Image Not Found"
-          className={`leftPumpkin ${pumpkinAnimation ? 'animatePumpkin' : ''}`}
+  }, []);
+  return (
+    <>
+      {isLoading && <div className="loaderWrapper">Loading...</div>}
+      <div className="pageWrapper">
+        <img src={bgImage} alt="" className="bgImage" />
+        <div>
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <motion.img
+          src={leftPumpkin}
+          alt="Image Not Found"
+          className={`leftPumpkin ${pumpkinAnimation ? "animatePumpkin" : ""}`}
           initial={false}
           animate={pumpkinAnimation ? { rotate: -30, x: -200 } : {}}
-          transition={{ duration: 1 }}/>
-          <motion.img src={rightPumpkin} alt="Image Not Found" className={`rightPumpkin ${pumpkinAnimation ? 'animatePumpkin' : ''}`}
-        initial={false}
-        animate={pumpkinAnimation ? { rotate: 30, x: 200 } : {}}
-        transition={{ duration: 1 }}/>
+          transition={{ duration: 1 }}
+          />
+        <motion.img
+          src={rightPumpkin}
+          alt="Image Not Found"
+          className={`rightPumpkin ${pumpkinAnimation ? "animatePumpkin" : ""}`}
+          initial={false}
+          animate={pumpkinAnimation ? { rotate: 30, x: 200 } : {}}
+          transition={{ duration: 1 }}
+        />
 
-          <div className="mainContent">
-            {/* {data.map(item => (
+        <div className="mainContent">
+          {/* {data.map(item => (
           <div
           key={item.id}
             className={`stuccan ${activeStuccan === item.id ? 'active' : ''}`}
@@ -171,30 +180,30 @@ function App() {
             </div>
           </div>
         ))} */}
-        <AnimatePresence initial={false} mode="wait">
-          {selectedStuccan && (
-            <motion.div
-              key={selectedStuccan.id}
-              className="stuccan"
-              initial={{ opacity: 0}}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0}}
-              transition={{duration:1}}
-            >
-              <div className="photo">
-                <img src={selectedStuccan.photo} alt={selectedStuccan.name} />
-              </div>
-              <div className="text">
-                <span className="deptName">{selectedStuccan.dept}</span>
-                <span className="stuccanName">{selectedStuccan.name}</span>
-              </div>
-            </motion.div>
-          )}
+          <AnimatePresence initial={false} mode="wait">
+            {selectedStuccan && (
+              <motion.div
+                key={selectedStuccan.id}
+                className="stuccan"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1 }}
+              >
+                <div className="photo">
+                  <img src={selectedStuccan.photo} alt={selectedStuccan.name} />
+                </div>
+                <div className="text">
+                  <span className="deptName">{selectedStuccan.dept}</span>
+                  <span className="stuccanName">{selectedStuccan.name}</span>
+                </div>
+              </motion.div>
+            )}
           </AnimatePresence>
-          </div>
         </div>
-
-    </>;
+      </div>
+    </>
+  );
 }
 
 export default App;
